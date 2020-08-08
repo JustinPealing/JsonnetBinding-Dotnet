95aea568d0e05c889f2e85d5e0f3472e5bbc9798
Justin Pealing <justin@pealing.me.uk>
2020-08-08 14:14:08 +0100

    Add jsonnet_json functions to NativeMethods.

--- JsonnetBinding/NativeMethods.cs
+++ JsonnetBinding/NativeMethods.cs
@@ -5,129 +5,104 @@ namespace JsonnetBinding
 {
     /// <summary>
     /// PInvoke calls for libjsonnet. See jsonnet.h for reference:
-    ///
     /// https://github.com/google/jsonnet/blob/master/include/libjsonnet.h
     /// </summary>
     internal static class NativeMethods
     {
-        /// <summary>
-        /// Callback used to load imports.
-        /// </summary>
-        /// <remarks>
-        /// The returned char* should be allocated with jsonnet_realloc.  It will be cleaned up by
-        /// libjsonnet when no-longer needed.
-        /// </remarks>
-        /// <param name="ctx">User pointer, given in jsonnet_import_callback.</param>
-        /// <param name="baseDir">The directory containing the code that did the import.</param>
-        /// <param name="rel">The path imported by the code.</param>
-        /// <param name="foundHere">Set this byref param to path to the file, absolute or relative to the process's CWD.
-        /// This is necessary so that imports from the content of the imported file can be resolved correctly. Allocate
-        /// memory with jsonnet_realloc. Only use when *success = 1.</param>
-        /// <param name="success">Set this byref param to 1 to indicate success and 0 for failure.</param>
-        /// <returns>The content of the imported file, or an error message.</returns>
         public delegate IntPtr JsonnetImportCallback(
             IntPtr ctx, string baseDir, string rel, out IntPtr foundHere, out int success);
 
-        /// <summary>
-        /// Create a new Jsonnet virtual machine.
-        /// </summary>
+        public delegate IntPtr JsonnetNativeCallback(
+            IntPtr ctx, IntPtr[] argv, out bool success);
+        
         [DllImport("libjsonnet.so")]
         public static extern JsonnetVmHandle jsonnet_make();
 
-        /// <summary>
-        /// Set the maximum stack depth.
-        /// </summary>
         [DllImport("libjsonnet.so")]
-        public static extern void jsonnet_max_stack(JsonnetVmHandle vm, UInt32 v);
+        public static extern void jsonnet_max_stack(JsonnetVmHandle vm, uint v);
         
-        /// <summary>
-        /// Set the number of objects required before a garbage collection cycle is allowed.
-        /// </summary>
         [DllImport("libjsonnet.so")]
-        public static extern void jsonnet_gc_min_objects(JsonnetVmHandle vm, UInt32 v);
+        public static extern void jsonnet_gc_min_objects(JsonnetVmHandle vm, uint v);
         
-        /// <summary>
-        /// Run the garbage collector after this amount of growth in the number of objects.
-        /// </summary>
         [DllImport("libjsonnet.so")]
         public static extern void jsonnet_gc_growth_trigger(JsonnetVmHandle vm, double v);
         
-        /// <summary>
-        /// Expect a string as output and don't JSON encode it.
-        /// </summary>
         [DllImport("libjsonnet.so")]
-        public static extern void jsonnet_string_output(JsonnetVmHandle vm, Int32 v);
+        public static extern void jsonnet_string_output(JsonnetVmHandle vm, int v);
 
-        /// <summary>
-        /// Bind a Jsonnet external var to the given string.
-        /// </summary>
         [DllImport("libjsonnet.so")]
-        public static extern void jsonnet_ext_var(JsonnetVmHandle vm, string key, string value);
+        public static extern string jsonnet_json_extract_string(JsonnetVmHandle vm, IntPtr v);
 
-        /// <summary>
-        /// Bind a Jsonnet external var to the given code.
-        /// </summary>
         [DllImport("libjsonnet.so")]
-        public static extern void jsonnet_ext_code(JsonnetVmHandle vm, string key, string value);
+        public static extern bool jsonnet_json_extract_number(JsonnetVmHandle vm, IntPtr v, out double outVal);
 
-        /// <summary>
-        /// Bind a string top-level argument for a top-level parameter.
-        /// </summary>
         [DllImport("libjsonnet.so")]
-        public static extern void jsonnet_tla_var(JsonnetVmHandle vm, string key, string value);
-
-        /// <summary>
-        /// Bind a code top-level argument for a top-level parameter.
-        /// </summary>
-        [DllImport("libjsonnet.so")]
-        public static extern void jsonnet_tla_code(JsonnetVmHandle vm, string key, string value);
-
-        /// <summary>
-        /// Set the number of lines of stack trace to display (0 for all of them).
-        /// </summary>
-        [DllImport("libjsonnet.so")]
-        public static extern void jsonnet_max_trace(JsonnetVmHandle vm, UInt32 v);
+        public static extern int jsonnet_json_extract_bool(JsonnetVmHandle vm, IntPtr v);
         
-        /// <summary>
-        /// Evaluate a file containing Jsonnet code, return a JSON string.
-        /// </summary>
-        /// <param name="vm"></param>
-        /// <param name="filename">Path to a file containing Jsonnet code.</param>
-        /// <param name="error">Return whether or not there was an error.</param>
-        /// <returns>Either JSON or the error message.</returns>
         [DllImport("libjsonnet.so")]
-        public static extern IntPtr jsonnet_evaluate_file(JsonnetVmHandle vm, string filename, out bool error);
-        
-        /// <summary>
-        /// Evaluate a string containing Jsonnet code, return a JSON string.
-        /// </summary>
-        /// <param name="vm"></param>
-        /// <param name="filename">Path to a file (used in error messages).</param>
-        /// <param name="snippet">Jsonnet code to execute.</param>
-        /// <param name="error">Return whether or not there was an error.</param>
-        /// <returns>Either JSON or the error message.</returns>
-        [DllImport("libjsonnet.so")]
-        public static extern IntPtr jsonnet_evaluate_snippet(JsonnetVmHandle vm, string filename, string snippet, out bool error);
+        public static extern bool jsonnet_json_extract_null(JsonnetVmHandle vm, IntPtr v);
 
-        /// <summary>
-        /// Allocate, resize, or free a buffer.  This will abort if the memory cannot be allocated.  It will only
-        /// return NULL if sz was zero.
-        /// </summary>
-        /// <param name="buf">If NULL, allocate a new buffer. If an previously allocated buffer, resize it.</param>
-        /// <param name="sz">The size of the buffer to return. If zero, frees the buffer.</param>
-        /// <returns>The new buffer.</returns>
+        [DllImport("libjsonnet.so")]
+        public static extern IntPtr jsonnet_json_make_string(JsonnetVmHandle vm, string v);
+        
+        [DllImport("libjsonnet.so")]
+        public static extern IntPtr jsonnet_json_make_number(JsonnetVmHandle vm, double v);
+        
+        [DllImport("libjsonnet.so")]
+        public static extern IntPtr jsonnet_json_make_bool(JsonnetVmHandle vm, bool v);
+        
+        [DllImport("libjsonnet.so")]
+        public static extern IntPtr jsonnet_json_make_null(JsonnetVmHandle vm);
+        
+        [DllImport("libjsonnet.so")]
+        public static extern IntPtr jsonnet_json_make_array(JsonnetVmHandle vm);
+        
+        [DllImport("libjsonnet.so")]
+        public static extern void jsonnet_json_array_append(JsonnetVmHandle vm, IntPtr arr, IntPtr v);
+        
+        [DllImport("libjsonnet.so")]
+        public static extern IntPtr jsonnet_json_make_object(JsonnetVmHandle vm);
+        
+        [DllImport("libjsonnet.so")]
+        public static extern void jsonnet_json_object_append(JsonnetVmHandle vm, IntPtr obj, string f, IntPtr v);
+        
+        [DllImport("libjsonnet.so")]
+        public static extern void jsonnet_json_destroy(JsonnetVmHandle vm, IntPtr v);
+
         [DllImport("libjsonnet.so")]
         public static extern IntPtr jsonnet_realloc(JsonnetVmHandle vm, IntPtr buf, UIntPtr sz);
 
-        /// <summary>
-        /// Override the callback used to locate imports.
-        /// </summary>
         [DllImport("libjsonnet.so")]
         public static extern void jsonnet_import_callback(JsonnetVmHandle vm, JsonnetImportCallback cb, IntPtr ctx);
         
-        /// <summary>
-        /// Complement of <see cref="jsonnet_make"/>.
-        /// </summary>
+        [DllImport("libjsonnet.so")]
+        public static extern void jsonnet_native_callback(
+            JsonnetVmHandle vm, string name, JsonnetNativeCallback cb, IntPtr ctx, string[] parms);
+
+        [DllImport("libjsonnet.so")]
+        public static extern void jsonnet_ext_var(JsonnetVmHandle vm, string key, string value);
+
+        [DllImport("libjsonnet.so")]
+        public static extern void jsonnet_ext_code(JsonnetVmHandle vm, string key, string value);
+
+        [DllImport("libjsonnet.so")]
+        public static extern void jsonnet_tla_var(JsonnetVmHandle vm, string key, string value);
+
+        [DllImport("libjsonnet.so")]
+        public static extern void jsonnet_tla_code(JsonnetVmHandle vm, string key, string value);
+        
+        [DllImport("libjsonnet.so")]
+        public static extern void jsonnet_max_trace(JsonnetVmHandle vm, uint v);
+
+        [DllImport("libjsonnet.so")]
+        public static extern void jsonnet_jpath_add(JsonnetVmHandle vm, string v);
+        
+        [DllImport("libjsonnet.so")]
+        public static extern IntPtr jsonnet_evaluate_file(JsonnetVmHandle vm, string filename, out bool error);
+        
+        [DllImport("libjsonnet.so")]
+        public static extern IntPtr jsonnet_evaluate_snippet(JsonnetVmHandle vm, string filename, string snippet, out bool error);
+
         [DllImport("libjsonnet.so")]
         public static extern void jsonnet_destroy(IntPtr vm);
     }
